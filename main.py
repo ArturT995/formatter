3f6ccb7cac852b3873d769504c93699f053cf296
ArturT995 <arturtre95@gmail.com>
2025-07-25 22:48:54 +0300

    1. Initial setup and outline

---
+++ main.py
@@ -0,0 +1,33 @@
+import re
+
+
+path = "input.txt"
+
+
+
+def get_input(path):
+    with open(path) as f:
+        text = f.read()
+        return text
+
+def format_text(text):
+    output = "" 
+    
+    #strip links/images with regex.
+    
+    #remove headers
+    
+    #remove empty spaces
+
+    #add bold italic to ":?"
+    
+    with open(output) as f:
+        return f.write()
+
+
+    #detect input from clipboard and write it to input file
+    #send output to clipboard for pasting
+    #paste results to obsidian/note app.
+
+"- `sudo apt-get install xclip`to install the xclip utility."
+
